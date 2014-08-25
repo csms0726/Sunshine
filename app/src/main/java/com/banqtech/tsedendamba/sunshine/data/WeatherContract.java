771dcea16b3a087c48dd7602f2f208742d5c4722
Tsedendamba Batsaikhan <csms0726@gmail.com>
2014-08-25 12:45:04 +0800

    location entity update

--- app/src/main/java/com/banqtech/tsedendamba/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/banqtech/tsedendamba/sunshine/data/WeatherContract.java
@@ -52,11 +52,23 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
     }
-    public static final class Locationentry implements BaseColumns{
+
+    /* Inner class that defines the table contents of the location table */
+    public static final class LocationEntry implements BaseColumns {
+
+        // Table name
         public static final String TABLE_NAME = "location";
-        public static final String COLUMN_LOC_ID = "location_id";
-        public static final String COLUMN_LOC_SETTING = "location_setting";
-        public static final String COLUMN_CITYNAME = "cityname";
+
+        // The location setting string is what will be sent to openweathermap
+        // as the location query.
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // Human readable location string, provided by the API.  Because for styling,
+        // "Mountain View" is more recognizable than 94043.
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // In order to uniquely pinpoint the location on the map when we launch the
+        // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
     }
