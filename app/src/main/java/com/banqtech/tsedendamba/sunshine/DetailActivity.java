9c080499ff90b88cc496bd3eadb76d49603a89d1
Tsedendamba <csms0726@gmail.com>
2014-08-28 10:01:46 +0800

    Update Detail activity

--- app/src/main/java/com/banqtech/tsedendamba/sunshine/DetailActivity.java
+++ app/src/main/java/com/banqtech/tsedendamba/sunshine/DetailActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+//This is detail activity
 public class DetailActivity extends ActionBarActivity {
 
     @Override
